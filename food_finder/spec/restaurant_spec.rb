2ca2f848c744dadaadf576a81098cab6ca2df4b4
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2015-12-16 20:32:52 +0600

    finishes resturant_challenge

--- food_finder/spec/restaurant_spec.rb
+++ food_finder/spec/restaurant_spec.rb
@@ -6,12 +6,24 @@ describe Restaurant do
   let(:crescent) { Restaurant.new(:name => 'Crescent', :cuisine => 'paleo', :price => '321') }
   
   describe 'attributes' do
-  
-    it "allow reading and writing for :name"
+    
+    subject {Restaurant.new}
+    
+    it "allow reading and writing for :name" do
+      expect(subject.name).to eq ''  
+    end
 
-    it "allow reading and writing for :cuisine"
+    it "allow reading and writing for :cuisine" do
+      subject.name = "Honda"
+      
+      expect(subject.name).to eq 'Honda'
+    end
 
-    it "allow reading and writing for :price"
+    it "allow reading and writing for :price" do
+      subject.price = 5000
+      
+      expect(subject.price).to eq 5000
+    end
     
   end
   
@@ -46,19 +58,17 @@ describe Restaurant do
   describe '.all' do
     
     it 'returns array of restaurant objects from @@file' do
-      pending
       Restaurant.load_file(test_file)
       restaurants = Restaurant.all
-      expect(restaurants.class).to eq('FIXME')
-      expect(restaurants.length).to eq('FIXME')
-      expect(restaurants.first.class).to eq('FIXME')
+      expect(restaurants.class).to eq(Array)
+      expect(restaurants.length).to eq(6)
+      expect(restaurants.first.class).to eq(Restaurant)
     end
 
     it 'returns an empty array when @@file is nil' do
-      pending
       no_output { Restaurant.load_file(nil) }
       restaurants = Restaurant.all
-      expect(restaurants).to eq('FIXME')
+      expect(restaurants.size).to eq(0)
     end
     
   end
@@ -69,52 +79,152 @@ describe Restaurant do
       # subject would return the same thing
       let(:no_options) { Restaurant.new }
 
-      it 'sets a default of "" for :name'
+      it 'sets a default of "" for :name' do
+        expect(no_options.name).to eq ""
+      end
 
-      it 'sets a default of "unknown" for :cuisine'
+      it 'sets a default of "unknown" for :cuisine' do
+        expect(no_options.cuisine).to eq "unknown"
+      end
 
-      it 'does not set a default for :price'
+      it 'does not set a default for :price' do
+        expect(no_options.price).to be_nil
+      end
     end
     
     context 'with custom options' do
+      subject {Restaurant.new}
       
-      it 'allows setting the :name'
+      it 'allows setting the :name' do
+        subject.name = "New Name"
+        expect(subject.name).to eq "New Name"
+      end
 
-      it 'allows setting the :cuisine'
-
-      it 'allows setting the :price'
-
+      it 'allows setting the :cuisine' do
+        subject.cuisine = "Motorola"
+        expect(subject.cuisine).to eq "Motorola"
+      end
+      
+      it 'allows setting the :price' do
+        subject.price = 2000
+        expect(subject.price).to eq 2000
+      end
+      
     end
 
   end
   
   describe '#save' do
     
-    it 'returns false if @@file is nil'
+    subject {Restaurant.new}
     
-    it 'returns false if not valid'
+    it 'returns false if @@file is nil' do
+      # will auto set nil 
+      Restaurant.load_file('non_existing_file')
+      
+      expect(subject.save).to be false
+    end
     
-    it 'calls append on @@file if valid'
+    it 'returns false if not valid' do
+      subject.name = '' # make invalid
+
+      expect(subject.save).to be false
+    end
+    
+    it 'calls append on @@file if valid' do
+      # make a valid subject
+      subject.name = "Pizza-hut"
+      subject.cuisine = "Bangla"
+      subject.price = 123
+      
+      # spying for :append on Resturant.file i.e @@file
+      allow(Restaurant.file).to receive(:append)
+      
+      # call save on valid object
+      subject.save
+      
+      # expect the call is made
+      expect(Restaurant.file).to have_received(:append)
+    end
     
   end
   
   describe '#valid?' do
     
-    it 'returns false if name is nil'
-
-    it 'returns false if name is blank'
-
-    it 'returns false if cuisine is nil'
-
-    it 'returns false if cuisine is blank'
+    # a valid subject
+    subject {Restaurant.new(
+              name: 'Pizzahut',
+              cuisine: 'Bangladesh',
+              price: 123)}
     
-    it 'returns false if price is nil'
-
-    it 'returns false if price is 0'
+    before(:example) do
+      expect(subject.valid?).to be true
+    end
     
-    it 'returns false if price is negative'
+    it 'returns false if name is nil' do
+      # set name nil
+      subject.name = nil
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
 
-    it 'returns true if name, cuisine, price are present'
+    it 'returns false if name is blank' do
+      # set name to blank on valid subject
+      subject.name = ''
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+
+    it 'returns false if cuisine is nil' do
+      # set cuisine nil
+      subject.cuisine = nil
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+
+    it 'returns false if cuisine is blank' do
+      # set cuisine to blank on valid subject
+      subject.cuisine = ''
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+    
+    it 'returns false if price is nil' do
+      # set price to nil on valid subject
+      subject.price = nil
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+
+    it 'returns false if price is 0' do
+      # set price to 0 on valid subject
+      subject.price = 0
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+    
+    it 'returns false if price is negative' do
+      # set price to negative value on valid subject
+      subject.price = -60
+      
+      # expect false
+      expect(subject.valid?).to be false
+    end
+
+    it 'returns true if name, cuisine, price are present' do
+      restaurant = Restaurant.new
+      restaurant.name = "Bracket"
+      restaurant.cuisine = "Bangla"
+      restaurant.price = 123
+      
+      expect(restaurant.valid?).to be true
+    end
     
   end
 
